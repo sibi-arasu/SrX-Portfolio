747ef3706938c13a25b4557998b3e80ddf051c3f
SIBIARASU D <sibiarasu.cs20@bitsathy.ac.in>
2023-01-29 01:22:38 +0530

    test

---
+++ src/assets/index.js
@@ -0,0 +1,31 @@
+import robot from "./robot.png";
+import logo from "./logo.svg";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import arrowUp from "./arrow-up.svg";
+import instagram from "./instagram.svg";
+import linkedin from "./linkedin.svg";
+import twitter from "./twitter.svg";
+import avatar from "./avatar.gif";
+import skls from "./skls.svg";
+import github from "./github.svg";
+import OP1 from "./OP1.png";
+import git from "./git.png";
+import wp from "./wp.png";
+import mail from "./mail.png"
+
+export {
+  robot,
+  logo,
+  menu,
+  close,
+  arrowUp,
+  instagram,
+  linkedin,
+  twitter,
+   avatar,
+  skls,
+  github,
+  OP1,
+  git,wp,mail
+};
